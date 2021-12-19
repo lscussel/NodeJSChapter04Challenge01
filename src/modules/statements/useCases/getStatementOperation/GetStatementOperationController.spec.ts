af8943954e57c8cb94168f67090bc0d810f5cf9c
lscussel <root@DESKTOP-NAUB8AU.localdomain>
2021-12-18 22:33:32 -0300

    20211218 2233 finished challenge chapter 04

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationController.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationController.spec.ts
@@ -4,6 +4,10 @@ import  { Connection } from "typeorm";
 import { app } from "../../../../app";
 import createConnection from "@database/index";
 
+import { v4 as uuid } from "uuid";
+import authConfig from "@config/auth";
+import { sign } from "jsonwebtoken";
+
 let connection: Connection;
 
 describe("Get Statement Operation Controller", () => {
@@ -24,16 +28,77 @@ describe("Get Statement Operation Controller", () => {
     await connection.close();
   });
 
-  it("", async () => {
+  it("Should be able to get statement operation by id", async () => {
     const response = await request(app).post("/api/v1/sessions")
     .send({
       email: "supertest@teste.com.br",
       password: "123456"
     });
     const { token } = response.body;
+
+    const responseDeposit = await request(app).post("/api/v1/statements/deposit")
+    .send({
+      amount: 100,
+      description: "2021 November Allowance"
+    })
+    .set({
+      Authorization: `Bearer ${token}`
+    });
+
+    const { id } = responseDeposit.body;
+    const url = "/api/v1/statements/" + id;
+    const responseStatementOperation = await request(app).get(url)
+    .set({
+      Authorization: `Bearer ${token}`
+    });
+    const { type } = responseStatementOperation.body;
+
+    expect(responseStatementOperation.status).toBe(200);
+    expect(type).toBeDefined();
   });
 
-  it("", async () => {
+  it("Should not be able to get statement operation by a nonexistent id", async () => {
+    const response = await request(app).post("/api/v1/sessions")
+    .send({
+      email: "supertest@teste.com.br",
+      password: "123456"
+    });
+    const { token } = response.body;
 
+    const idOperation = uuid();
+    const url = "/api/v1/statements/" + idOperation;
+    const responseBalance = await request(app).get(url)
+    .set({
+      Authorization: `Bearer ${token}`
+    });
+    const { message } = responseBalance.body;
+
+    expect(responseBalance.status).toBe(404);
+    expect(message).toBe("Statement not found");
+  });
+
+  it("Should not be able to get statement operation from a non existent user", async () => {
+    const id = uuid();
+    const user = {
+      id,
+      name: "intruder",
+      email: "intruder@teste.com.br"
+    };
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({ user }, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    const idOperation = uuid();
+    const url = "/api/v1/statements/" + idOperation;
+    const responseBalance = await request(app).get(url)
+    .set({
+      Authorization: `Bearer ${token}`
+    });
+    const { message } = responseBalance.body;
+
+    expect(responseBalance.status).toBe(404);
+    expect(message).toBe("User not found");
   });
 });
